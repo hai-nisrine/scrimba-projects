1709c31c7c3c1ed49cc0115ec017516581707311
Nisrine Haida <nisrine.haida.77@gmail.com>
2026-07-20 15:07:58 +0100

    linked it to firebase

--- leads-tracker-app/index.js
+++ leads-tracker-app/index.js
@@ -1,56 +1,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
-import { firebaseConfig } from "./firebase-config";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+import { firebaseConfig } from "./firebase-config.js";
 
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
-
-
-
-
-
-
-
-let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 const inputBtn = document.querySelector("#input-btn")
 const deleteBtn = document.querySelector("#delete-btn")
-const tabBtn = document.querySelector("#tab-btn")
 
+const refInDB = ref(database, "leads")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
+onValue(refInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+   
+})
 
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(refInDB, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-    render(myLeads)
+    
 })
 
 deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    myLeads.length = 0
-    render(myLeads)
+    remove(refInDB)
+    ulEl.innerHTML = ""
+   
 })
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-        render(myLeads)
-    })
 
-    
-})
 
 
 function render(leads) {
